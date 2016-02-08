57a243880f45b22d1ad69202d89d1c76219b8860
citibeth <rpf2116@columbia.edu>
2016-02-07 22:09:16 -0500

    LazyPtr created and tested.

--- slib/ibmisc/memory.hpp
+++ slib/ibmisc/memory.hpp
@@ -22,6 +22,7 @@
 
 #include <memory>
 #include <typeinfo>
+#include <boost/variant.hpp>
 
 namespace ibmisc {
 
@@ -59,10 +60,72 @@ inline std::shared_ptr<T_DEST> dynamic_shared_cast(std::unique_ptr<T_SRC> &&src)
 	return std::shared_ptr<T_DEST>(dynamic_cast_unique_ptr<T_DEST, T_SRC>(std::move(src)));
 }
 
+// ---------------------------------------------------------------
+
+/** Stores a value that is lazily evaluted.  The evaluation function
+may return either a object (and "owned" reference), or a C-pointer (a
+"borrowed" reference). */
+template<class TypeT>
+class LazyPtr {
+	TypeT *_ptr;						// Borrowed reference
+	std::unique_ptr<TypeT> _uptr;	// Owned reference
+
+	boost::variant<
+		std::function<TypeT *()>,		// Borrowed
+		std::function<std::unique_ptr<TypeT> ()>	// Owned
+	> _compute;
+
+
+public:
+
+	/** Constructs with an already-evaluted borrowed reference. */
+	LazyPtr(TypeT *ptr) : _ptr(ptr) {}
+
+	/** Constructs with an already-evaluated owned refernece. */
+	LazyPtr(std::unique_ptr<TypeT> &&uptr) : _uptr(std::move(uptr)) {
+		_ptr = _uptr.get();
+	}
+
+	/** Constructs with a function to produce an owned reference. */
+	LazyPtr(std::function<std::unique_ptr<TypeT> ()> &&compute_owned)
+		: _ptr(0), _compute(std::move(compute_owned)) {}
+
+	/** Constructs with a function to produce a borrowed referene. */
+	LazyPtr(std::function<TypeT *()> &&compute_borrowed)
+		: _ptr(0), _compute(std::move(compute_borrowed)) {}
+
+	// -----------------------------------------------------
+	// Used by operator*()
+	class eval_visitor : public boost::static_visitor<> {
+		LazyPtr<TypeT> const *lptr;
+	public:
+		eval_visitor(LazyPtr<TypeT> const *_lptr) : lptr(_lptr) {}
+
+		void operator()(std::function<TypeT *()> const &borrowed_fn) const
+		{
+			const_cast<TypeT *&>(lptr->_ptr) = borrowed_fn(); }
+		void operator()(std::function<std::unique_ptr<TypeT> ()> const &owned_fn) const {
+			const_cast<std::unique_ptr<TypeT>&>(lptr->_uptr) = owned_fn();
+			const_cast<TypeT *&>(lptr->_ptr) = &*lptr->_uptr;
+		}
+	};
+	friend class eval_visitor;
+
+
+	/** Dereference our "pointer."  Evaluates the function, if it has
+	not already been evaluated. */
+	TypeT &operator*() const {
+		if (!_ptr) {
+			boost::apply_visitor(eval_visitor(this), _compute);
+		}
+		return *_ptr;
+	}
+	TypeT *operator->() const
+		{ return &operator*(); }
+};
+// --------------------------------------------------------------
+
 }	// namespace ibmisc
-
-
-
 
 
 
---
+++ tests/ibmisc/test_memory.cpp
@@ -0,0 +1,66 @@
+// https://github.com/google/googletest/blob/master/googletest/docs/Primer.md
+
+#include <gtest/gtest.h>
+#include <ibmisc/memory.hpp>
+#include <iostream>
+#include <cstdio>
+#include <memory>
+#include <map>
+
+using namespace std;
+using namespace ibmisc;
+
+// The fixture for testing class Foo.
+class MemoryTest : public ::testing::Test {
+protected:
+	// You can do set-up work for each test here.
+	MemoryTest() {}
+
+	// You can do clean-up work that doesn't throw exceptions here.
+	virtual ~MemoryTest() {}
+
+	// If the constructor and destructor are not enough for setting up
+	// and cleaning up each test, you can define the following methods:
+
+	// Code here will be called immediately after the constructor (right
+	// before each test).
+	virtual void SetUp() {}
+
+	// Code here will be called immediately after each test (right
+	// before the destructor).
+	virtual void TearDown() {}
+
+//	  // The mock bar library shaed by all tests
+//	  MockBar m_bar;
+};
+
+string hello = "hello";
+string *get_hello()
+{
+	return &hello; 
+}
+unique_ptr<string> new_hello()
+	{ return unique_ptr<string>(new string("hello")); }
+
+TEST_F(MemoryTest, lazy_ptr_test)
+{
+	LazyPtr<string> p0(get_hello());
+	LazyPtr<string> p1(new_hello());
+	LazyPtr<string> p2(&get_hello);
+	LazyPtr<string> p3(&new_hello);
+
+	EXPECT_EQ("hello", *p0);
+	EXPECT_EQ("hello", *p1);
+	EXPECT_EQ("hello", *p2);
+	EXPECT_EQ("hello", *p3);
+}
+
+
+// -----------------------------------------------------------
+// -----------------------------------------------------------
+
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
