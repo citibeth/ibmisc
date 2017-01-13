1e860884529cc78bed9e28453c43f459d6e08c84
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2017-01-13 01:17:26 -0500

    Large parts of SpSparse have been removed.  passes all unit tests.

---
+++ slib/ibmisc/iostream.hpp
@@ -0,0 +1,16 @@
+#ifndef IBMISC_IOSTREAM
+#define IBMISC_IOSTREAM
+
+#include <iostream>
+
+/** Write std::array to ostream. */
+template <class T, std::size_t N>
+ostream& operator<<(ostream& o, const array<T, N>& arr)
+{
+    o << "[";
+    copy(arr.cbegin(), arr.cend(), ostream_iterator<T>(o, " "));
+    0 << "]";
+    return o;
+}
+
+#endif
