380044e78b3264babcba4267cba682045e46bdf0
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2018-01-16 15:17:41 -0500

    Got rid of obsolete hashing code

--- slib/ibmisc/hash.hpp
+++ slib/ibmisc/hash.hpp
@@ -23,24 +23,6 @@
 #include <boost/functional/hash.hpp>
 #endif
 
-#if 0
-namespace ibmisc {
-
-/** Utility class to hash std::pair, which was left out of std::hash.
-Used to construct std::unordered_maps where std::pair is used as the
-key.  Might have been made obsolete by an implementation of
-std::hash<> for std::pair.
-@see gridutil.hpp,std::hash< pair< S, T > > */
-template<class A, class B>
-struct HashPair {
-    size_t operator()(std::pair<A, B> const &x) const throw()
-        { return std::hash<A>()(x.first)*31 + std::hash<B>()(x.second); }
-};
-
-}
-#endif
-
-
 /**@file std::hash<std::pair<>> is defined in "namespace std" to fix a shortcoming in C++.
 
 @see: http://stackoverflow.com/questions/7222143/unordered-map-hash-function-c
