7bd5022c05cd7f94626034d29f8ea5c1e501cec2
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2018-02-26 19:20:12 -0500

    runlength online encode/decode works.

---
+++ slib/spsparse/vector.hpp
@@ -0,0 +1,27 @@
+#ifndef SPSPARSE_VECTOR_HPP
+#define SPSPARSE_VECTOR_HPP
+
+#include <vector>
+
+namespace spsparse {
+namespace accum {
+
+template<class ValueT>
+class Vector {
+public:
+    typedef ValueT val_type;
+private:
+    std::vector<ValueT> &vec;
+public:
+    Vector(std::vector<ValueT> &_vec) : vec(_vec) {}
+    void add(ValueT const &val)
+        { vec.push_back(val); }
+};
+
+template<class ValueT>
+inline Vector<ValueT> vector(std::vector<ValueT> &vals)
+    { return Vector<ValueT>(vals); }
+
+}}    // namespace
+
+#endif
