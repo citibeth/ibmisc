635a25516365a8eb3997f02f7cda3d7d81edfe7b
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2018-03-14 14:23:22 -0400

    Python module builds and imports

--- pylib/ibmisc_cython.cpp
+++ pylib/ibmisc_cython.cpp
@@ -1,3 +1,11 @@
+#include <cstdio>
+#include <algorithm>
+#include <ibmisc/netcdf.hpp>
+#include <ibmisc/cython.hpp>
+#include <spsparse/SparseSet.hpp>
+#include "ibmisc_cython.hpp"
+
+static double const NaN = std::numeric_limits<double>::quiet_NaN();
 
 namespace ibmisc {
 namespace cython {
@@ -14,7 +22,7 @@ PyObject *linear_Weighted_apply_weight(
     int dim,
     PyObject *A_s_py)            // A_b{nj_s} One row per variable
 {
-    auto shape(self->shape());
+    auto shape(self.shape());
 
     // |j_s| = size of sparse input vector space (A_s)
     // |j_d] = size of dense input vector space (A_d)
@@ -26,12 +34,12 @@ PyObject *linear_Weighted_apply_weight(
 
     // Allocate output
     PyObject *B_s_py = ibmisc::cython::new_pyarray<double,1>(
-        std::array<int,2>{n_n});
-    auto B_s(np_to_blitz<double1>(B_s_py, "B_s_py", {-1,-1}));
-    B_s = nan;
+        std::array<int,1>{n_n});
+    auto B_s(np_to_blitz<double,1>(B_s_py, "B", {-1}));
+    B_s = NaN;
 
     // Run it!
-    self->apply_weight(dim, A_s, B_s, true);
+    self.apply_weight(dim, A_s, B_s, true);
 
     return B_s_py;
 }
@@ -51,13 +59,14 @@ PyObject *linear_Weighted_apply_M(
     // auto accum_type(parse_enum<AccumType>(saccum_type));
 
     // Allocate output
+    auto shape(self.shape());
     PyObject *B_s_py = ibmisc::cython::new_pyarray<double,2>(
         std::array<int,2>{n_n, shape[0]});
     auto B_s(np_to_blitz<double,2>(B_s_py, "B_s_py", {-1,-1}));
-    B_s = fill
+    B_s = fill;
 
     // Run it!
-    self->apply_M(A_s, B_s, AccumType::REPLACE, force_conservation);
+    self.apply_M(A_s, B_s, linear::AccumType::REPLACE, force_conservation);
 
     return B_s_py;
 }
